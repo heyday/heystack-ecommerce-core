8e2678fd50032acc37f2b6b5efc76c3edee9dfe5
Stevie Mayhew <stevie@heyday.co.nz>
2012-07-09 10:19:01 +1200

    FEATURE: automatic generation of storage files for silverstripe objects

--- code/SilverStripe/core/code/EcommerceInputController.php
+++ code/SilverStripe/core/code/EcommerceInputController.php
@@ -1,7 +1,27 @@
 <?php
+/**
+ * This file is part of the Heystack package
+ * 
+ * @package Heystack
+ */
 
+/**
+ * Heystack\Subsystem\Core\Storage\DataObjectCodeGenerator namespace
+ */
 use Heystack\Subsystem\Core\ServiceStore;
 
+/**
+ * EcommerceInputController handles all input to the system.
+ * 
+ * Calls which interact with the ecommerce system should be directed through
+ * this controller.
+ * 
+ * @copyright  Heyday
+ * @author Stevie Mayhew <stevie@heyday.co.nz>
+ * @author Cameron Spiers <cam@heyday.co.nz>
+ * @package Heystack
+ * 
+ */
 class EcommerceInputController extends Controller
 {
 
@@ -11,6 +31,9 @@ class EcommerceInputController extends Controller
     private $inputHandlerService;
     private $outputHandlerService;
 
+    /**
+     * Setup this controller.
+     */
     public function __construct()
     {
 
@@ -22,6 +45,12 @@ class EcommerceInputController extends Controller
 
     }
 
+    /**
+     * Process the request to the controller and direct it to the correct input
+     * and output controllers via the input and output processor services.
+     * 
+     * @return mixed
+     */
     public function process()
     {
 
