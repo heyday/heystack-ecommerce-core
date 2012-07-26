3ccc5ba320d940709e00a1a9720ae5c409051911
glenn-bautista <glenn@heyday.co.nz>
2012-07-27 10:19:50 +1200

    FEATURE: added Services definition class

---
+++ code/Heystack/Subsystem/Ecommerce/Services.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This file is part of the Ecommerce-Core package
+ *
+ * @package Ecommerce-Core
+ */
+
+/**
+ * Shipping namespace
+ */
+namespace Heystack\Subsystem\Shipping;
+
+/**
+ * Holds constants corresponding to the services defined in the services.yml file
+ *
+ * @copyright  Heyday
+ * @author Glenn Bautista <glenn@heyday.co.nz>
+ * @package Ecommerce-Core
+ */
+final class Services
+{    
+    /**
+     * Holds the identfier of the transaction
+     * For use with the ServiceStore::getService($identifier) call
+     */
+    const TRANSACTION = 'transaction';
+    
+    /**
+     * Holds the identifier of the transaction subscriber
+     * For use with the ServiceStore::getService($identifier) call
+     */
+    const TRANSACTION_SUBSCRIBER = 'transaction_subscriber';
+    
+    /**
+     * Holds the identifier of the currency service
+     * For use with the ServiceStore::getService($identifier) call
+     */
+    const CURRENCY_SERVICE = 'currency_service';
+    
+    /**
+     * Holds the identifier of the currency subscriber
+     * For use with the ServiceStore::getService($identifier) call
+     */
+    const CURRENCY_SUBSCRIBER = 'currency_subscriber';
+    
+    /**
+     * Holds the identifier of the currency input processor
+     * For use with the ServiceStore::getService($identifier) call
+     */
+    const CURRENCY_INPUT_PROCESSOR = 'currency_input_processor';
+    
+    /**
+     * Holds the identifier of the currency output processor
+     * For use with the ServiceStore::getService($identifier) call
+     */
+    const CURRENCY_OUTPUT_PROCESSOR = 'currency_output_processor';
+}
