9927e7502029ad068dd49253709add4650bdf38c
Stevie Mayhew <stevie@heyday.co.nz>
2012-08-07 12:42:19 +1200

    FEATURE: added pricing decorator

---
+++ code/SilverStripe/currency/code/decorators/PricingDecorator.php
@@ -0,0 +1,42 @@
+<?php
+
+use Heystack\Subsystem\Core\ServiceStore;
+
+class PricingDecorator extends DataObjectDecorator
+{
+    
+    public function extraStatics()
+    {
+        
+        $db = array();
+        
+        $currencyService = ServiceStore::getService(\Heystack\Subsystem\Ecommerce\Services::CURRENCY_SERVICE);
+        
+        foreach ($currencyService->getCurrencies() as $currency) {
+            
+            $db[$currency->getCurrencyCode()."Price"] = 'Decimal(10, 2)';
+            
+        }
+        
+        return array(
+            'db' => $db
+        );
+    }
+    
+    public function updateCMSFields(\FieldSet &$fields)
+    {
+        
+        $currencyService = ServiceStore::getService(\Heystack\Subsystem\Ecommerce\Services::CURRENCY_SERVICE);
+        
+        foreach ($currencyService->getCurrencies() as $currency) {
+            
+            $fields->removeByName($currency->getCurrencyCode()."Price");
+            
+            $fields->addFieldToTab('Root.Prices', new NumericField($currency->getCurrencyCode()."Price"));
+            
+        }
+        
+        return $fields;
+    }
+    
+}
